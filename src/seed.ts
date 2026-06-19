27a1538390083a0cf3b9059ee6bfd46e2d3e1ab0
Odko4010 <Odko4010@gmail.com>
2026-06-19 17:42:10 +0800

    fix: seed.ts import path

--- src/seed.ts
+++ src/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from './generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 import * as dotenv from 'dotenv'
 dotenv.config()
