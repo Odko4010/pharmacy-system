9bbfa7fc1cf227babc206faa29575143f0257f53
Odko4010 <Odko4010@gmail.com>
2026-06-20 16:33:37 +0800

    fix: add trustHost to NextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   session: {
     strategy: "jwt",
   },
@@ -20,24 +21,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         if (!credentials?.email || !credentials?.password) {
           return null;
         }
-
         const user = await prisma.user.findUnique({
           where: { email: credentials.email as string },
         });
-
         if (!user || !user.isActive) {
           return null;
         }
-
         const isValidPassword = await bcrypt.compare(
           credentials.password as string,
           user.password
         );
-
         if (!isValidPassword) {
           return null;
         }
-
         return {
           id: user.id,
           email: user.email,
