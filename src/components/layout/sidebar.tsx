178375ab60d0659a4e72743c857b90c5119a1a77
Odko4010 <Odko4010@gmail.com>
2026-06-29 09:31:22 +0800

    fix: restore sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import { UserCircle, usePathname } from "next/navigation";
-import { UserCircle,
+import { usePathname } from "next/navigation";
+import {
   LayoutDashboard, Pill, ShoppingCart, Receipt, Boxes,
-  Users, Tag, BarChart2, PackagePlus, RotateCcw, Truck
+  Users, Tag, BarChart2, PackagePlus, RotateCcw, Truck, UserCircle
 } from "lucide-react";
 import clsx from "clsx";
 
